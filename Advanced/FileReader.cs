97fa45f6f0cba8168e245f0bb8de1cb91c9b16c5
pbullhove <peterbullhove@gmail.com>
2022-05-06 14:53:43 +0200

    Add project files.

---
+++ Advanced/FileReader.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvancedProgram
+{
+    public interface IFileReader
+    {
+        string ReadFile(string fileName);
+    }
+
+    public class FileReader : IFileReader
+    {
+        public string ReadFile(string fileName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Basic/Calculator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace BasicProgram
+{
+    public class Calculator
+    {
+        public int? Add(int? a, int? b)
+        {
+            if (a == null || b == null)
+                throw new ArgumentNullException();
+
+            return a + b;
+        }
+
+        
+    }
+
+}
